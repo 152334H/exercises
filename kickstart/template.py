4ebfe8a010901c1597f9a89c6deca19a55af67cc
152334H <54623771+152334H@users.noreply.github.com>
2022-04-25 17:01:15 +0800

    templates

---
+++ kickstart/template.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3.8
+from itertools import *
+from operator import *
+from collections import *
+from dataclasses import dataclass
+dd,ctr = defaultdict, Counter
+
+_pow = pow
+from math import *
+pow = _pow # math's pow() doesn't provide the `mod` optional arg
+
+from re import findall
+from functools import reduce, lru_cache, singledispatchmethod
+
+# non-generator versions of common funcs
+Map = lambda f,it: list(map(f,it))
+Filter = lambda f,it: list(filter(f,it))
+Reversed = lambda ls: list(reversed(ls))
+Zip = lambda *ls: list(zip(*ls))
+
+# for the package
+from typing import *
+
